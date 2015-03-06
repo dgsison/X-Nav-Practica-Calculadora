be66478caa3e92ca77ef1991382fc0420252cbc9
Dgsison <dgsison@Ubuntu-Vaio.(none)>
2015-03-07 00:07:04 +0100

    terminado

--- script.js
+++ script.js
@@ -18,37 +18,79 @@ $(document).ready(function(){
             $(".screen").html("");
         });
 
-        
-
         $(document).keydown(function(e){
                             
             switch(e.keyCode){
                 case 96:
                 case 48:
                     var n = 0;
-                    $(".screen").append(n);
-                    $(".outcome").val($(".outcome").val() + n);
-                    alert("HOooola");
                     break;
                 case 97:
                 case 49:
                     var n = 1;
-                    $(".screen").append(n);
-                    $(".outcome").val($(".outcome").val() + n);
                     break;
                 case 98:
-                case 49:
-                    n = 2;
+                case 50:
+                    var n = 2;
                     break;
+                case 99:
+                case 51:
+                   var n =3;
+                    break;
+                case 100:
+                case 52:
+                    var n = 4;
+                    break;
+                case 101:
+                case 53:
+                    var n = 5;
+                    break;
+                case 102:
+                case 54:
+                    var n = 6;
+                    break;
+                case 103:
+                case 55:
+                    var n = 7;
+                    break;
+                case 104:
+                case 56:
+                    var n = 8;
+                    break;
+                case 105:
+                case 57:
+                    var n = 9;
+                    break;
+                case 106:
+                     var n = "*";
+                    break;
+
                 case 107:
-                    n = "+";
-                    $(".screen").append(n);
-                    $(".outcome").val($(".outcome").val() + n);
+                    var n = "+";
+                    break;
+                case 109:
+                    var n = "-";
+                    break;
+                case 110:
+                    var n = ".";
+                    break;
+                case 111:
+                    var n = "/";
+                    break;
+                case 46:
+                    $(".outcome").val("");
+                    $(".screen").html("");
                     break;
                 case 13:
                     $(".outcome").val(eval($(".outcome").val()));
                     $(".screen").html(eval($(".outcome").val()));
+                    break;
+            }
+            
 
+            if(!(e.keyCode == 46) && !(e.keyCode == 13)){
+                $(".screen").append(n);
+                $(".outcome").val($(".outcome").val() + n);
             }
         });
     })
